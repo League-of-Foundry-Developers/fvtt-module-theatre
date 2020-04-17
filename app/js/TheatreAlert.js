1f3f010b7c6de35cbf979319b114c4257e936074
U~man <ewolfwood@yandex.com>
2020-04-17 11:44:43 +0200

    FIX: changed game.modules for 0.5.4

--- app/js/TheatreAlert.js
+++ app/js/TheatreAlert.js
@@ -54,7 +54,7 @@ class TheatreAlert extends Application {
 	async render(force=false, options={}) {
 		let data = await fetch("modules/theatre/motd.json").then(r => r.text()); 
 		//let data = await this.makeGETRequest("https://gitlab.com/Ayanzo/motds/raw/master/motd_theatre.json"); 
-		let module = game.modules.find(m => m.id == "theatre"); 
+		let module = game.modules.get("theatre"); 
 		let motdNewInfo = Theatre.instance.settings.motdNewInfo; 
 
 		try {
