139c5139eee5ebb23671ed71a84dba15f71e8eba
Samuel Lopez <samulopez@gmail.com>
2025-09-10 15:11:44 +0200

    feat: new setting to show more than 5 actors on top of screen

--- src/scripts/constants/constants.js
+++ src/scripts/constants/constants.js
@@ -1,21 +1,22 @@
 const CONSTANTS = {
-  MODULE_ID: "theatre",
-  PATH: `modules/theatre/`,
-  PREFIX_I18N: `Theatre`,
-  FLAGS: {},
-  API: {
-    EVENT_TYPE: {
-      sceneevent: "sceneevent",
-      typingevent: "typingevent",
-      resyncevent: "resyncevent",
-      reqresync: "reqresync",
+    MODULE_ID: "theatre",
+    PATH: `modules/theatre/`,
+    PREFIX_I18N: `Theatre`,
+    FLAGS: {},
+    API: {
+        EVENT_TYPE: {
+            sceneevent: "sceneevent",
+            typingevent: "typingevent",
+            resyncevent: "resyncevent",
+            reqresync: "reqresync",
+        },
     },
-  },
-  SOCKET: "module.theatre",
-  NARRATOR: "Narrator",
-  ICONLIB: "modules/theatre/assets/graphics/emotes",
-  DEFAULT_PORTRAIT: "icons/mystery-man.png",
-  PREFIX_ACTOR_ID: "theatre-",
+    SOCKET: "module.theatre",
+    NARRATOR: "Narrator",
+    ICONLIB: "modules/theatre/assets/graphics/emotes",
+    DEFAULT_PORTRAIT: "icons/mystery-man.png",
+    PREFIX_ACTOR_ID: "theatre-",
+    MAX_NUM_ACTORS_ON_CONTROL_GROUP: 5,
 };
 
 CONSTANTS.PATH = `modules/${CONSTANTS.MODULE_ID}/`;
